d5cf51e6e923a57ee91e8eb060c2e73eb20f1032
U-DOMENE\490 <490@490SP31.domene.local>
2017-05-12 19:51:38 +0200

    Players::Players() + getKoordFromPlayer og setKoord....

--- Players.h
+++ Players.h
@@ -1,9 +1,15 @@
 
-
+#include "Rute.h"
 
 class Players {
 	
-	
+		Rute_verdi rute_verdi;
+		bool isComputer;
+		sendKoordsFromPlayer(int, int);
+	public:
+		Players(bool comp, Rute_verdi rv): isComputer{comp}, rute_verdi{rv} {}
+		virtual std::pair<int, int> getKoordsFromPlayer();
+			
 	
 	
 };
